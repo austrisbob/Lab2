9727663dddb09d3a3ae5b45aecd1eae0ee4e051d
austris1 <152059689+austris1@users.noreply.github.com>
2024-10-30 17:17:32 +0200

    Add project files.

---
+++ Lab2.DataAccess/Department.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.DataAccess
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Nosaukums { get; set; }
+        [Required]
+        [StringLength(50)]
+        public int DarbiniekuSK { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Apraksts { get; set; }
+        public List<Employee> Employee { get; } = new();
+    }
+}
---
+++ Lab2.DataAccess/DepartmentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab2.DataAccess
+{
+    public class DepartmentDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DepartmentDbContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer(
+                "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\austr\\source\\repos\\Lab2.DataAccess\\Lab2.DataAccess\\DepartmentDb.mdf;Integrated Security=True");
+
+    }
+}
