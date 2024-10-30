30b3c85c2bd40292557e6b759718e58e71b91f5c
austris1 <152059689+austris1@users.noreply.github.com>
2024-10-30 18:39:26 +0200

    8

--- Lab2.Console/Program.cs
+++ Lab2.Console/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Lab2.Console
+﻿using Lab2.DataAccess;
+using Lab2.DataAccess.Migrations;
+using Microsoft.Data.SqlClient;
+using System.Diagnostics.Metrics;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Lab2.Consoles
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var connection = new SqlConnection("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False");
+            connection.Open();
+            /*
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT";
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(i: 0));
+            }*/
+            connection.Close();
         }
     }
+
 }
